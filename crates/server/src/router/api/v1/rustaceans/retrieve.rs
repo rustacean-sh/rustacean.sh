0c75abeb36c6e59b0e8f6bb2a9ac184955993b79
Luciano Ramello <luchosr@gmail.com>
2024-12-04 23:04:55 +0100

    feat: add `luchosr` to rustaceans endpoint (#2)

--- crates/server/src/router/api/v1/rustaceans/retrieve.rs
+++ crates/server/src/router/api/v1/rustaceans/retrieve.rs
@@ -7,7 +7,13 @@ pub async fn handler() -> Response {
         name: "Esteban Borai".to_string(),
         image: "https://avatars.githubusercontent.com/u/34756077?v=4".into(),
         github_url: "https://github.com/EstebanBorai".into(),
-    }];
+    },
+    Rustacean {
+        name: "Luciano Ramello".to_string(),
+        image: "https://avatars.githubusercontent.com/u/20213274?v=4".into(),
+        github_url: "https://github.com/luchosr".into(),
+        }
+    ];
 
     Json(objects).into_response()
 }
