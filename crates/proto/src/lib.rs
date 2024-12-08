ca3396fea2c14de8fba575a194de8cc90e4150eb
Luciano Ramello <luchosr@gmail.com>
2024-12-08 12:57:20 +0100

    feat(proto):  extend Rustacean struct

--- crates/proto/src/lib.rs
+++ crates/proto/src/lib.rs
@@ -1,8 +1,47 @@
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct Rustacean {
+pub struct Rustacean<'a> {
     pub name: String,
-    pub image: String,
-    pub github_url: String,
+    pub gh_user: String,
+    pub image: Option<&'a str>,
+    pub social_networks: Option<Vec<SocialNetwork>>,
+    pub geographical_location: Option<Location>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum SocialNetwork {
+    Instagram { username: String },
+    X { username: String },
+    Custom { url: Url },
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Location {
+    continent: Continent,
+    country: Country,
+    city: City,
+    timezone: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum Continent {
+    NorthAmerica,
+    SouthAmerica,
+    Europe,
+    Asia,
+    Africa,
+    Oceania,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+
+pub struct Country {
+    name: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct City {
+    name: String,
 }
