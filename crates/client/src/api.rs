a9d10adf943080fa219a988d1fbe29eaf78851bb
Luciano Ramello <luchosr@gmail.com>
2024-12-20 22:21:17 +0100

    fix(client): improve api

--- crates/client/src/api.rs
+++ crates/client/src/api.rs
@@ -1,7 +1,6 @@
-use anyhow::{Ok, Result};
+use anyhow::Result;
 use reqwest::Url;
 use serde::Deserialize;
-use serde_json;
 use serde_json::Value;
 
 #[derive(Deserialize, Debug)]
@@ -9,14 +8,12 @@ pub struct Stars {
     pub amount: Option<u64>,
 }
 
-const API_REPO_URL = "https://api.github.com/repos/rustacean-sh/rustacean.sh";
+impl Stars {
     pub async fn get_amount() -> Result<Self> {
-        let url = format!(API_REPO_URL);
+        let url = format!("https://api.github.com/repos/rustacean-sh/rustacean.sh");
         let url = Url::parse(&*url)?;
         let res = reqwest::get(url).await?.text().await?;
         let v: Value = serde_json::from_str(res.as_str()).unwrap();
-
-        // leptos::logging::log!("Stars: {}", v["stargazers_count"]);
 
         Ok(Stars {
             amount: v["stargazers_count"].as_u64(),
