3afd51667cf0948227c004d8fd881dd502694952
Luciano Ramello <luchosr@gmail.com>
2024-12-20 12:52:38 +0100

    fix(client): improve url repo

--- crates/client/src/api.rs
+++ crates/client/src/api.rs
@@ -1,31 +1,25 @@
-use exitfailure::ExitFailure;
-use proto::Rustacean;
+use anyhow::{Ok, Result};
 use reqwest::Url;
 use serde::Deserialize;
+use serde_json;
+use serde_json::Value;
+
 #[derive(Deserialize, Debug)]
 pub struct Stars {
     pub amount: Option<u64>,
 }
 
-impl Stars {
-    pub async fn get_amount() -> Result<Self, ExitFailure> {
-        let url = format!(
-            "https://api.github.com/repos/{owner}/{repo}/stargazers",
-            owner = "rustacean-sh",
-            repo = "rustacean.sh"
-        );
+const API_REPO_URL = "https://api.github.com/repos/rustacean-sh/rustacean.sh";
+    pub async fn get_amount() -> Result<Self> {
+        let url = format!(API_REPO_URL);
         let url = Url::parse(&*url)?;
-        let res = reqwest::get(url)
-            .await?
-            .json::<Vec<Rustacean>>()
-            .await?
-            .len();
+        let res = reqwest::get(url).await?.text().await?;
+        let v: Value = serde_json::from_str(res.as_str()).unwrap();
 
-        // let res = reqwest::get(url).await?.text().await?;
-        leptos::logging::log!("Stars: {}", res);
+        // leptos::logging::log!("Stars: {}", v["stargazers_count"]);
 
         Ok(Stars {
-            amount: Some(6 as u64),
+            amount: v["stargazers_count"].as_u64(),
         })
     }
 }
