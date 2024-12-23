2feb8721f157654966434a071933a091afb83f4a
Luciano Ramello <luchosr@gmail.com>
2024-12-23 10:57:27 +0100

    fix(client): improve get_amount method

--- crates/client/src/api.rs
+++ crates/client/src/api.rs
@@ -10,13 +10,11 @@ pub struct Stars {
 
 impl Stars {
     pub async fn get_amount() -> Result<Self> {
-        let url = format!("https://api.github.com/repos/rustacean-sh/rustacean.sh");
+        let url = "https://api.github.com/repos/rustacean-sh/rustacean.sh";
         let url = Url::parse(&*url)?;
         let res = reqwest::get(url).await?.text().await?;
-        let v: Value = serde_json::from_str(res.as_str()).unwrap();
+        let v: Stars = serde_json::from_str(res.as_str())?;
 
-        Ok(Stars {
-            amount: v["stargazers_count"].as_u64(),
-        })
+        Ok(Stars { amount: v.amount })
     }
 }
