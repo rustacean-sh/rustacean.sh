d23f234dbadf386e1f76d69787e003bb1bb8340c
Luciano Ramello <luchosr@gmail.com>
2025-01-02 16:42:03 +0100

    refactor(client): add header component on layout

--- crates/client/src/components/atoms/github_stars.rs
+++ crates/client/src/components/atoms/github_stars.rs
@@ -20,14 +20,13 @@ pub fn GitHubStars() -> impl IntoView {
                 }
                 Err(err) => {
                     leptos::logging::error!("{err}");
-                    // error.set(Some(err.to_string()));
                 }
             }
         });
     });
 
     view! {
-        <div id="github-stars" class="flex items-center gap-x-1 md:gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
+         <div id="github-stars" class="flex items-center gap-x-1 md:gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
             <a href="https://github.com/rustacean-sh/rustacean.sh"  target="_blank" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-md bg-white border border-gray-200 text-black hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none ">
             <figure >
                 <img  height="15" width="15" src={"../assets/images/star-svgrepo-com.svg"} alt={"Star in github"} />
